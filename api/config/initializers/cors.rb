56d4300e7e23f332ba6d67bdbd0ea8f0b5907af5
Your Name <Your Email>
2025-10-18 16:31:42 -0400

    Fix CORS configuration for preflight requests

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -3,7 +3,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         origins "https://mai-omega.vercel.app", "http://localhost:3000"
 
         resource "*",
+        headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true # Important for cookies!
+        credentials: true,
+        expose: ["Authorization"]
     end
 end
